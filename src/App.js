c504246412db1e148b37f6727bc28355b92d4660
Dwaine Simmons <dwainesimmons88@gmail.com>
2018-10-29 23:24:05 -0400

    comments for apps

--- src/App.js
+++ src/App.js
@@ -29,34 +29,36 @@ class App extends Component {
 
 
 
-
+// var for the google maps fucntion. This will create the map
     let promiseGM = load_google_maps();
+// car for the foursquare API venue function. This will get an locations json
     let promisePlaces = load_places();
+// this creates an promise for the google maps and foursquare venues
 
     Promise.all([
       promiseGM,
       promisePlaces
     ])
+// After the promise we get the results for the google maps and venues
     .then(results => {
 
     let google = results[0];
     let venues = results[1].response.venues;
     this.google = google;
 
-
-
+// this is to get the center for your google maps
 
     var myLatLng = {lat: venues[0].location.lat, lng: venues[0].location.lng };
-
+// this is to create the map element
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: myLatLng
   });
-
+// var for the markers
   const pins = [];
-
+// sets the infowindow to a varible
   var infowindow = new google.maps.InfoWindow();
-
+// I used the array for the venues to create markers for the google maps
 venues.forEach(markers => {
   var marker = new google.maps.Marker({
     position:{lat: markers.location.lat, lng: markers.location.lng},
@@ -67,16 +69,16 @@ venues.forEach(markers => {
     title: markers.name,
     id: markers.id
   });
-
+// this moves the markers to the pins array
   pins.push(marker);
-
+// this sets the state for the pins array
   this.setState({
         pins: pins
       });
   console.log(pins);
 
 
-
+// When you mouseover an marker, it will bounce
   google.maps.event.addListener(marker, 'mouseover', () =>{
 
         if (marker.getAnimation() !== null) {
@@ -105,7 +107,10 @@ venues.forEach(markers => {
       window.setTimeout( () => {
       map.panTo(marker.position);
     }, 1000)
+
+    // sets the content for the infowindow
       infowindow.setContent(markers.name);
+      // if you click the marker the infowindow will open
       infowindow.open(map, marker);
 
     }
@@ -121,7 +126,7 @@ venues.forEach(markers => {
     }
     */
 })
-
+// this sets the state of the venues
 this.setState({ venues })
 
 console.log(venues);
