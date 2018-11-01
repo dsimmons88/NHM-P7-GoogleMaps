8dcb48dce266e5ebc53e1c3db64e755c6d645bde
Dwaine Simmons <dwainesimmons88@gmail.com>
2018-10-31 22:34:56 -0400

    deleted unused componenet

---
+++ src/SearchInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import MapSidebar from './Sidebar';
+import App from './App';
+
+export default class MarkerSearch extends Component {
+  render() {
+    return (
+      <div>
+      <MapSidebar />
+      <ul>
+      </div>
+      );
+    }
+  }
