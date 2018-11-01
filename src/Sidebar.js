697c5452d2fdd3580f4e868d69008862a6d5d10e
Dwaine Simmons <dwainesimmons88@gmail.com>
2018-10-31 23:07:39 -0400

    clear out comments

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -35,67 +35,6 @@ venueItemClick = venue => {
 
 
 
-/*
-//  this is a the function for the onClick for the list item
-venueItemClick = venues => {
-  let markers = this.props.pins.filter(m => m.id === marker.id)
-  this.state.map.setZoom(15);
-  this.state.map.setCenter(marker.getPosition());
-  // 1 seconds after the center of the map has changed, pan back to the
-  // marker.
-  window.setTimeout( () => {
-  this.state.map.panTo(marker.position);
-}, 1000)
-
-
-// sets the content for the infowindow
-  this.state.infowindow.setContent(marker.name);
-  // if you click the marker the infowindow will open
-  this.state.infowindow.open(this.props.map, marker);
-// set animation for the click for item list
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(this.state.google.maps.Animation.BOUNCE);
-  }
-  setTimeout(() => { marker.setAnimation(null); }, 1000);
-}
-
-
-/*
-// a function to filter the markers to match the query
-  filterMarkers = (query) => {
-
-    if(this.state.query === ""){
-    this.setState({filterMarkers: this.props.venes})
-  }
-
-// this is for the filter list.
-let l = this.props.pins.filter(marker => marker.name.toLowerCase().includes(query.toLowerCase()));
-// this sort the markers to match the query
-      this.props.pins.forEach(marker => {
-      if (marker.name.toLowerCase().includes(query.toLowerCase()) === true )
-      {marker.setVisible(true);
-     //searchMarkers.push(marker);
-
-
-      } else {
-      marker.setVisible(false)
-
-    }
-
-
-
-    });
-// to update the query state
-    this.setState({ query });
-// to update the filter list state
-  this.setState({filterMarkers: l})
-  //  console.log(filterMarkers);
-  }
-
-*/
-
 render () {
   return (
     <div>
